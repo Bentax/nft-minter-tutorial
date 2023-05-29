d75ebbcfe7dcf286ce905b05bf7a507b9ef5e002
bentax <bentaxrus@mail.ru>
2023-05-29 11:46:44 +0300

    Added packages

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  connectWallet,
-  getCurrentWalletConnected //import here
-} from "./utils/interact.js";
-
+import { connectWallet, getCurrentWalletConnected, mintNFT } from "./utils/interact.js";
 
 const Minter = (props) => {
 
@@ -17,8 +13,34 @@ const Minter = (props) => {
   useEffect(async () => {
     const {address, status} = await getCurrentWalletConnected();
     setWallet(address)
-    setStatus(status); 
+    setStatus(status);
+    addWalletListener(); 
 }, []);
+
+function addWalletListener() {
+  if (window.ethereum) {
+    window.ethereum.on("accountsChanged", (accounts) => {
+      if (accounts.length > 0) {
+        setWallet(accounts[0]);
+        setStatus("👆🏽 Write a message in the text-field above.");
+      } else {
+        setWallet("");
+        setStatus("🦊 Connect to Metamask using the top right button.");
+      }
+    });
+  } else {
+    setStatus(
+      <p>
+        {" "}
+        🦊{" "}
+        <a target="_blank" href={`https://metamask.io/download.html`}>
+          You must install Metamask, a virtual Ethereum wallet, in your
+          browser.
+        </a>
+      </p>
+    );
+  }
+}
 
   const connectWalletPressed = async () => {
     const walletResponse = await connectWallet();
@@ -27,9 +49,11 @@ const Minter = (props) => {
   };
 
 
-  const onMintPressed = async () => { //TODO: implement
-    
-  };
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
+};
+
 
   return (
     <div className="Minter">
